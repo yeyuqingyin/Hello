2b704e7ff63a475def48900ba78bbb9a1463daad
21cn <21cn@21cndeiMac.local>
2018-08-08 19:48:48 +0800

    second commit

---
+++ Hello/Hello.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDelegate
+//
+//  Created by 21cn on 2018/7/26.
+//  Copyright © 2018年 21cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Hello : UIResponder
+
+@property (strong, nonatomic) NSString * n;
+
+
+@end
+
---
+++ Hello/Hello.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  TestDelegate
+//
+//  Created by 21cn on 2018/7/26.
+//  Copyright © 2018年 21cn. All rights reserved.
+//
+
+#import "Hello.h"
+
+@interface Hello ()
+
+@end
+
+@implementation Hello
+
+
+
+
+@end
